18f6839d4a1bc4d34a9d3cc21164ce2ebee57ec5
Paul Huisman <paulhuisman88@gmail.com>
2014-10-22 14:22:05 +0200

    Updated JS so that you can use multiple fields at the same time

--- js/flickr-acf5.js
+++ js/flickr-acf5.js
@@ -2,61 +2,58 @@
 
 	function initialize_flickrfield( $el ) {
 
-		$('.field_form.flickr_field').each(function() {
-			function fill_input_value() {
-				var active_items = new Array();
+		function fill_input_value() {
+			var active_items = new Array();
 
-				$('.flickr_row.active-row').each(function() {
-					if ($(this).hasClass('photo_image')) {
-						active_items.push({
-							id: $(this).attr('data-flickr-id'), 
-							server: $(this).attr('data-flickr-server'), 
-							secret: $(this).attr('data-flickr-secret'), 
-							farm: $(this).attr('data-flickr-farm'), 
-							title: $(this).attr('data-flickr-title')
-						});	
-					} 
-					else {
-						active_items.push($(this).attr('data-flickr-id'));	
-					}
-					
-				});
-
-				if (active_items.length > 0) {
-					input.val(JSON.stringify(active_items));
-				}
-			} 
-
-			var self = $(this).parent(),
-				input;
-			
-			$('select option:selected', self).each(function() {
-				value = $(this).val();
-			});
-
-			$('.flickr_row', self).click(function(e) {
-				e.preventDefault();
-				// Deselect if active
-				if ($(this).hasClass('active-row')) {
-					$(this).removeClass('active-row');
-				}
+			$('.flickr_row.active-row', $el).each(function() {
+				if ($(this).hasClass('photo_image')) {
+					active_items.push({
+						id: $(this).attr('data-flickr-id'), 
+						server: $(this).attr('data-flickr-server'), 
+						secret: $(this).attr('data-flickr-secret'), 
+						farm: $(this).attr('data-flickr-farm'), 
+						title: $(this).attr('data-flickr-title')
+					});	
+				} 
 				else {
-					$(this).addClass('active-row');
+					active_items.push($(this).attr('data-flickr-id'));	
 				}
-
-				fill_input_value();
+				
 			});
-			
-			// Make hidden input with flickr data
-			input = $('<input />').attr('type', 'hidden').val(value).attr('name',$('select', self).attr('name')).addClass('flickr-id');	
 
-			// Remove default select
-			$('select', self).after(input).remove();	
-			
+			if (active_items.length > 0) {
+				input.val(JSON.stringify(active_items));
+			}
+		} 
+
+		var input;
+
+		$('select option:selected', $el).each(function() {
+			value = $(this).val();
+		});
+
+		$('.flickr_row', $el).click(function(e) {
+			e.preventDefault();
+			// Deselect if active
+			if ($(this).hasClass('active-row')) {
+				$(this).removeClass('active-row');
+			}
+			else {
+				$(this).addClass('active-row');
+			}
+
 			fill_input_value();
 		});
+
+		// Make hidden input with flickr data
+		input = $('<input />').attr('type', 'hidden').val(value).attr('name',$('select', $el).attr('name')).addClass('flickr-id');	
+
+		// Remove default select
+		$('select', $el).after(input).remove();	
+
+		fill_input_value();
 	
-	}
+  }
 
 	/*
 	*  ready append (ACF5)
